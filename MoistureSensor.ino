b40d3916550b0cd348ea60e233844d2af3b607f4
halesjo <43683606+halesjo@users.noreply.github.com>
2018-09-28 21:38:15 +0100

    Add files via upload
    added fully working 7 segment display and alert led stays on

--- MoistureSensor.ino
+++ MoistureSensor.ino
@@ -1,44 +1,51 @@
-/*  Automatic pumping system
-    This sketch was orignally written by SparkFun Electronics
-    Joel Bartlett 
-    August 31, 2015
-    Adapted by Josie Hales
-    August 25, 2018
+#include "SevSeg.h"
+SevSeg sevseg; 
 
-    This sketch will make a pump water crops if the mositure level is too low in the soil
-*/
 
 int val = 0; //value for storing moisture value 
-int soilPin = A0;//Declare a variable for the soil moisture sensor 
-int soilPower = 7;//Soil moisture Power pin
-int pumpPower = 8;//Pump Relay pin
+int soilPin = A5;//Declare a variable for the soil moisture sensor 
+int soilPower = 13;//Soil moisture Power pin
+int counter = 0;
+int pumpPower = 0;//Pump Relay pin
 
 int level = 0; //level of moisture 1023 = dry, 0 = wet
-int threshold = 800; //threshold for water to be pumped
-int checkingDelay = 1000; //delay between checking soil moisture level
-int pumpingTime = 10000; //length of time pumping water
+int threshold = 550; //threshold for water to be pumped
+int checkingDelay = 5000; //delay between checking soil moisture level
+int pumpingTime = 5000; //length of time pumping water
 
-void setup() 
-{
-  Serial.begin(9600);   // open serial over USB
+void setup(){
+  byte numDigits = 4;
+  byte digitPins[] = {9, 10, 11, 12};
+  byte segmentPins[] = {2, 3, 4, 5, 6, 7, 8};
 
-  pinMode(soilPower, OUTPUT);//Set D7 as an OUTPUT
+  bool resistorsOnSegments = true; 
+  bool updateWithDelaysIn = true;
+  byte hardwareConfig = COMMON_CATHODE; 
+  sevseg.begin(hardwareConfig, numDigits, digitPins, segmentPins, resistorsOnSegments);
+  sevseg.setBrightness(90);
+
+  pinMode(soilPower, OUTPUT);//Set D0 as an OUTPUT
+  pinMode(pumpPower, OUTPUT);//Set as an OUTPUT
   digitalWrite(soilPower, LOW);//Set to LOW so no power is flowing through the sensor
   digitalWrite(pumpPower, LOW);//Set to LOW so no power is flowing to the pump relay
 }
 
-void loop() 
-{
-Serial.print("Soil Moisture = ");    
-//get soil moisture value from the function below and print it
-level = readSoil();
-Serial.println(level);
-checkLevel(level);
-
-delay(checkingDelay);//take a reading every second
+void loop(){
+    Serial.print("Soil Moisture = ");    
+    //get soil moisture value from the function below and print it
+    level = readSoil();
+    Serial.println(level);
+    checkLevel(level);
+    counter = checkingDelay;
+    while (counter > 0)
+    {
+      sevseg.setNumber(level);
+      sevseg.refreshDisplay(); 
+      delay(5);
+      counter--;
+    }
+    
 }
-
-
 
 //This is a function used to get the soil moisture content
 int readSoil()
@@ -52,10 +59,12 @@ int readSoil()
 }
 void checkLevel(int level)
 {
-  if(level > threshold){
+  while(level > threshold){
     digitalWrite(pumpPower, HIGH);//turn D8 "On"
     delay(pumpingTime);
-    digitalWrite(pumpPower, LOW);//turn D8 "Off"
+    level = readSoil();
   }
-
+  digitalWrite(pumpPower, LOW);//turn D8 "Off"
+  delay(5);
+  level = readSoil();
 }
